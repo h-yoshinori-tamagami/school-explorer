7067acad31ecc030d0cdc23e04922c5c7576d676
yoshinori.tamagami <tamagamiy@gmail.com>
2026-04-18 10:56:18 +0900

    feat: 初回の静的サイトエクスポートを追加

---
+++ out/_next/static/chunks/app/not-found-9f764d976a4350f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{3763:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6831,23))}},e=>{e.O(0,[831,726,198,358],()=>e(e.s=3763)),_N_E=e.O()}]);
